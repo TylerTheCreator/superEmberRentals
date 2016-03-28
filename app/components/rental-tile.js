a81e9505a288a750b044fb14114755053a8b067b
Tim Durward and Tyler Finley <tylerfinleyy@gmail.com>
2016-03-28 10:43:27 -0700

    add delete record:

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isImageShowing: false,
+  actions: {
+    imageShow: function() {
+      this.set('isImageShowing',true);
+    },
+    imageHide: function() {
+      this.set('isImageShowing', false);
+    }
+  },
+  actions: {
+    delete(rental) {
+      if (confirm('Are you sure you want to delete this rental?')) {
+        this.sendAction('destroyRental', rental);
+      }
+    }
+  }
 });
