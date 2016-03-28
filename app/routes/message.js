5025ecbfb46d8b95cf423d91154e9329ab93bb90
Tim Durward and Tyler Finley <tim+tyler@>
2016-03-28 15:13:52 -0700

    add delete button for announcements

--- app/routes/message.js
+++ app/routes/message.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('message');
+  },
+  actions: {
+    destroyMessage(message) {
+      message.destroyRecord();
+      this.transitionTo('message');
+    }
   }
 });
